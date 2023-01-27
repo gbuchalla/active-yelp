9663cdada69551e855b1ba75a05905bc36e259f4
Guilherme Buchalla Ferreira <guilhermebuchallaferreira@gmail.com>
2023-01-26 21:54:26 -0300

    feat: add basic gym CRUD route handlers and templates; create gym CRUD

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 const Gym = require('./models/gym');
@@ -20,36 +21,52 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+// Homepage route
+app.get('/', (req, res) => {
+    res.send('Homepage');
+});
 
 // Gyms routes
 app.get('/gyms', async (req, res) => {
-    res.send('index route');
+    const allGyms = await Gym.find({});
+    res.render('index', { gyms: allGyms });
 });
 
 app.get('/gyms/new', (req, res) => {
-    res.send('index route') 
+    res.render('new');
 });
 
 app.post('/gyms', async (req, res) => {
-    // create gym route handler
+    const newGym = new Gym(req.body.gym);
+    await newGym.save();
+    res.redirect('/gyms');
 });
 
 app.get('/gyms/:id', async (req, res) => {
-    res.send('index route')  
+    const { id } = req.params;
+    const foundGym = await Gym.findById(id);
+    res.render('show', { gym: foundGym });
 });
 
 app.get('/gyms/:id/edit', async (req, res) => {
-    res.send('index route') 
+    const { id } = req.params;
+    const foundGym = await Gym.findById(id);
+    res.render('edit', { gym: foundGym });
 })
 
 app.put('/gyms/:id', async (req, res) => {
-    // update gym route handler
+    const { id } = req.params;
+    await Gym.findByIdAndUpdate(id, req.body.gym)
+    res.redirect(`/gyms/${id}`);
 });
 
 app.delete('/gyms/:id', async (req, res) => {
-    // delete gym route handler
+    const { id } = req.params;
+    await Gym.findByIdAndDelete(id);
+    res.redirect('/gyms');
 });
 
 // Server config
