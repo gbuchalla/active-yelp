b0cbd2b329c587646c101a9e486e4e8bfb6ca395
Guilherme Buchalla Ferreira <guilhermebuchallaferreira@gmail.com>
2023-01-27 19:22:33 -0300

    feat: create error handling functions for async route handlers; create new error class

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 const Gym = require('./models/gym');
+const ExpressError = require('./utils/newExpressError');
 
 
 // Setup e tratamento de erros da conexão com a database
@@ -24,52 +25,75 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+// Funções middleware
+function catchAsync(asyncFunction) {
+    return function (req, res, next) {
+        asyncFunction(req, res, next).catch(err => next(err));
+    };
+};
+
 // Homepage route
 app.get('/', (req, res) => {
     res.send('Homepage');
 });
 
 // Gyms routes
-app.get('/gyms', async (req, res) => {
+app.get('/gyms', catchAsync(async (req, res) => {
     const allGyms = await Gym.find({});
     res.render('index', { gyms: allGyms });
-});
+}));
 
 app.get('/gyms/new', (req, res) => {
     res.render('new');
 });
 
-app.post('/gyms', async (req, res) => {
-    const newGym = new Gym(req.body.gym);
-    await newGym.save();
-    res.redirect('/gyms');
-});
+app.post('/gyms', catchAsync(async (req, res, next) => {
+    if (Object.values(req.body.gym).some((data) => data === '')) {
+        next(new ExpressError(400, 'Dado inserido da academia considerado inválido'));
+    } else {
+        const newGym = new Gym(req.body.gym);
+        await newGym.save();
+        res.redirect(`/gyms/${newGym._id}`);
+    }
+}));
 
-app.get('/gyms/:id', async (req, res) => {
+app.get('/gyms/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const foundGym = await Gym.findById(id);
     res.render('show', { gym: foundGym });
-});
+}));
 
-app.get('/gyms/:id/edit', async (req, res) => {
+app.get('/gyms/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params;
     const foundGym = await Gym.findById(id);
     res.render('edit', { gym: foundGym });
-})
+}));
 
-app.put('/gyms/:id', async (req, res) => {
+app.put('/gyms/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Gym.findByIdAndUpdate(id, req.body.gym)
     res.redirect(`/gyms/${id}`);
-});
+}));
 
-app.delete('/gyms/:id', async (req, res) => {
+app.delete('/gyms/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Gym.findByIdAndDelete(id);
     res.redirect('/gyms');
+}));
+
+// Middleware de tratamento de erro
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError(404, 'Página não encontrada.'))
 });
 
-// Server config
+app.use((err, req, res, next) => {
+    const { name, status = 500, message = 'Ops, algo deu errado.', stack } = err;
+    res.status(status).send(`Error Message: ${message}... Error Stack: ${stack}`);
+    console.log(`\nError\nName: ${name}\nStatus: ${status}\nMessage: ${message}\nStack: ${stack}\n\n Full Error:\n`, err);
+});
+
+// Configuração do servidor
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servindo na porta ${port}`);
