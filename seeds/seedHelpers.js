cebcfb2eafebfad48de19eaed9d1b46a90e22aea
Guilherme Buchalla Ferreira <guilhermebuchallaferreira@gmail.com>
2023-02-13 22:56:20 -0300

    feat: add seed data generator

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,30 @@
+module.exports.descriptors = [
+    'Top',
+    'Sport',
+    'Start',
+    'Power',
+    'Iron',
+    'Open',
+    'Neo',
+    'Nova',
+    'Total',
+    'Alpha',
+    'Sigma',
+    'Premium',
+    'Studio',
+    'Espaço',
+    'Performance',
+    'Max',
+    'Bio',
+    'Free'
+]
+
+module.exports.places = [
+    'Fit',
+    'Village',
+    'Fitness',
+    'Academia',
+    'Gym',
+    'Athletic Center',
+    'Center Fitness'
+]
